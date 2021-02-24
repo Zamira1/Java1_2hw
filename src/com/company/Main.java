0e1d44983ce0948735eedca459692f7e28261a97
Zamira <zbaktybekova@gmail.com>
2021-02-24 20:22:59 +0300

    done

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,27 +5,27 @@ import java.util.Random;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(myMethod(getRandomAge(), -11));
-        System.out.println(myMethod(getRandomAge(), 28));
-        System.out.println(myMethod(getRandomAge(), 20));
-        System.out.println(myMethod(getRandomAge(), 5));
-        System.out.println(myMethod(getRandomAge(), 55));
+        System.out.println(myMethod(generateRandomAge(), -11));
+        System.out.println(myMethod(generateRandomAge(), 28));
+        System.out.println(myMethod(generateRandomAge(), 20));
+        System.out.println(myMethod(generateRandomAge(), 5));
+        System.out.println(myMethod(generateRandomAge(), 55));
 
     }
+
     public static String myMethod(int age, int temperature) {
         if (age >= 20 && age <= 45 && temperature >= -10 && temperature <= 30) {
-            System.out.println("Можно идти гулять");
+            return "Можно идти гулять";
         } else if (age < 20 && temperature <= 0 && temperature <= 28) {
-            System.out.println("Можно идти гулять");
+            return "Можно идти гулять";
         } else if (age > 45 && temperature > -10 && temperature <= 25) {
-            System.out.println("Можно идти гулять");
+            return "Можно идти гулять";
         } else {
-            System.out.println("Оставайся дома");
+            return "Оставайся дома";
         }
-        return "Возраст = " + age + " Температура на улице = " + temperature + "C";
     }
 
-    public static int getRandomAge() {
+    public static int generateRandomAge() {
         int maxAge = 71;
         Random random = new Random();
         return random.nextInt(maxAge);
